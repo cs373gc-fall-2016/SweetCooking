9197ca17b4e92a8df33c30f3a6a99c98899f2d61
Rafik Rizik <rizik944@gmail.com>
2016-10-14 23:02:39 -0500

    updated db with more models

--- app/models.py
+++ app/models.py
@@ -6,3 +6,24 @@ class Ingredient(db.Model):
     price = db.Column(db.Integer)
     info = db.Column(db.String(200))
     quantity = db.Column(db.Integer)
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), primary_key=True)
+    price = db.Column(db.Integer)
+    info = db.Column(db.String(200))
+    origin = db.Column(db.String(100))
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), primary_key=True)
+    price = db.Column(db.Integer)
+    data = db.Column(db.String(200))
+    description = db.Column(db.String(200))
+
+class LifeStyle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    weight_management = db.Column(db.String(80), primary_key=True)
+    price = db.Column(db.Integer)
+    info = db.Column(db.String(200))
+    quantity = db.Column(db.Integer)
