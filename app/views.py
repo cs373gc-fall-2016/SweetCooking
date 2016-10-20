01d82de9f8450d5b5e7b61b33272c78c6cb5b59b
jakemayo254 <jakemayo254@gmail.com>
2016-10-19 21:29:17 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
-def home():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
