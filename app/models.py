30e71d0644f61cc3da57f6b02a9860531f83721f
Kris Vandercook <cyberwyvern696@gmail.com>
2016-10-25 11:20:09 -0500

    new models

--- app/models.py
+++ app/models.py
@@ -1,28 +1,43 @@
 from app import db
 
-# join table for ingredients and products
-iptable = db.Table('iptable',
+# join table for ingredients and an ingredient list
+iitable = db.Table('iitable',
         db.Column('ing_id', db.Integer, db.ForeignKey('ingredient.id')),
-        db.Column('pro_id', db.Integer, db.ForeignKey('product.id')))
+        db.Column('inglist_id', db.Integer, db.ForeignKey('ingredientlist.id')))
 
-# join table for ingredients and recipes
-irtable = db.Table('irtable',
+# join table for ingredients and lifestyles
+iltable = db.Table('iltable',
         db.Column('ing_id', db.Integer, db.ForeignKey('ingredient.id')),
-        db.Column('rec_id', db.Integer, db.ForeignKey('recipe.id')))
+        db.Column('lif_id', db.Integer, db.ForeignKey('lifestyle.id')))
+
+# join table for recipes and lifestyles
+rltable = db.Table('rltable',
+        db.Column('rec_id', db.Integer, db.ForeignKey('recipe.id')),
+        db.Column('lif_id', db.Integer, db.ForeignKey('lifestyle.id')))
+
+# join table for products and lifestyles
+pltable = db.Table('pltable',
+        db.Column('pro_id', db.Integer, db.ForeignKey('product.id')),
+        db.Column('lif_id', db.Integer, db.ForeignKey('lifestyle.id')))
 
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    price = db.Column(db.Integer)
-    season = db.Column(db.String(80))
-    products = db.relationship('Product', secondary=iptable, backref='ingredients', lazy='dynamic')
-    recipes = db.relationship('Recipe', secondary=irtable, backref='ingredients', lazy='dynamic')
-    nutrition = db.relationship('Nutrition', backref='ingredient', lazy='dynamic', uselist=False)
+    img = db.column(db.String(200))
+    calories = db.Column(db.Integer)
+    protein = db.Column(db.Integer)
+    fat = db.Column(db.Integer)
+    carbs = db.Column(db.Integer)
+    ingredientlists = db.relationship('Ingredientlist', secondary=iitable, backref='ingredients', lazy='dynamic')
+    lifestyles = db.relationship('Lifestyle', secondary=iltable, backref='ingredients', lazy='dynamic')
 
-    def __init__(self, name, price=0, season='none'):
+    def __init__(self, name, img='', calories=0, protein=0, fat=0, carbs=0):
         self.name = name
-        self.price = price
-        self.season = season
+        self.img = img
+        self.calories = calories
+        self.protein = protein
+        self.fat = fat
+        self.carbs = carbs
 
     def __repr__(self):
         return 'Ingredient: {}'.format(self.name)
@@ -30,17 +45,19 @@ class Ingredient(db.Model):
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    price = db.Column(db.Integer)
-    origin = db.Column(db.String(100))
-    cooktime = db.Column(db.Integer)
-    nutrition = db.relationship('Nutrition', backref='recipe', lazy='dynamic', uselist=False)
-    lifestyle = db.relationship('Lifestyle', backref='recipe', lazy='dynamic', uselist=False)
+    img = db.Column(db.String(200))
+    time = db.Column(db.Integer)
+    instructions = db.Column(db.String(1000))
+    servings = db.Column(db.Integer)
+    ingredientlist = db.relationship('Ingredientlist', backref='recipe', uselist=False)
+    lifestyles = db.relationship('Lifestyle', secondary=rltable, backref='recipes', lazy='dynamic')
 
-    def __init__(self, name, price=0, origin='unknown', cooktime=0):
+    def __init__(self, name, img='', time=0, instructions='', servings=0):
         self.name = name
-        self.price = price
-        self.origin = origin
-        self.cooktime = cooktime
+        self.img = img
+        self.time = time
+        self.instructions = instructions
+        self.servings = servings
 
     def __repr__(self):
         return 'Recipe: {}'.format(self.name)
@@ -48,68 +65,72 @@ class Recipe(db.Model):
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    price = db.Column(db.Integer)
-    description = db.Column(db.String(200))
-    similarproducts = db.relationship('Product', lazy='dynamic')
-    nutrition = db.relationship('Nutrition', backref='product', lazy='dynamic', uselist=False)
-    lifestyle = db.relationship('Lifestyle', backref='product', lazy='dynamic', uselist=False)
+    img = db.Column(db.String(200))
+    servingsize = db.Column(db.Integer)
+    calories = db.Column(db.Integer)
+    protein = db.Column(db.Integer)
+    fat = db.Column(db.Integer)
+    satfat = db.Column(db.Integer)
+    transfat = db.Column(db.Integer)
+    carbs = db.Column(db.Integer)
+    sugar = db.Column(db.Integer)
+    lifestyles = db.relationship('Lifestyle', secondary=pltable, backref='products', lazy='dynamic')
 
-    def __init__(self, name, price=0, description='unknown'):
+    def __init__(self, name, img='', servingsize=0, calories=0, protein=0, fat=0, satfat=0,
+                 transfat=0, carbs=0, sugar=0):
         self.name = name
-        self.price = price
-        self.description = description
+        self.img = img
+        self.servingsize = servingsize
+        self.calories = calories
+        self.protein = protein
+        self.fat = fat
+        self.satfat = satfat
+        self.transfat = transfat
+        self.carbs = carbs
+        self.sugar = sugar
 
     def __repr__(self):
         return 'Product: {}'.format(self.name)
 
 class Lifestyle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    img = db.Column(db.String(200))
+    description = db.Column(db.String(500))
     weight_management = db.Column(db.Boolean)
-    dietary_restriction = db.Column(db.String(80))
     gluten_free = db.Column(db.Boolean)
-    carb_free = db.Column(db.Boolean)
+    ketogenic = db.Column(db.Boolean)
     vegetarian = db.Column(db.Boolean)
     vegan = db.Column(db.Boolean)
+    cheap = db.Column(db.Boolean)
+    dairy_free = db.Column(db.Boolean)
 
-    def __init__(self, weight_management=False, dietary_restriction='none', gluten_free=False,
-                 carb_free=False, vegetarian=False, vegan=False):
+    def __init__(self, name, img='', description='', weight_management=False,  gluten_free=False,
+                 ketogenic=False, vegetarian=False, vegan=False, cheap=False, dairy_free=False):
+        self.name = name
+        self.img = img
+        self.description = description
         self.weight_management = weight_management
-        self.dietary_restriction = dietary_restriction
         self.gluten_fee = gluten_free
-        self.carb_free = carb_free
+        self.ketogenic = ketogenic 
         self.vegetarian = vegetarian
         self.vegan = vegan
+        self.cheap = cheap
+        self.dairy_free = dairy_free
 
     def __repr__(self):
-        return 'Lifestyle: {}'.format(self.id)
+        return 'Lifestyle: {}'.format(self.name)
 
-class Nutrition(db.Model):
+class Ingredientlist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    serving_size = db.Column(db.String(80))
-    calories = db.Column(db.Integer)
-    fat = db.Column(db.Integer)
-    protein = db.Column(db.Integer)
-    carbs = db.Column(db.Integer)
+    amount = db.Column(db.Integer)
+    unit = db.Column(db.String(10))
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
 
-    def __init__(self, serving_size='none', calories=0, fat=0, protein=0, carbs=0):
-        self.serving_size = serving_size
-        self.calories = calories
-        self.fat = fat
-        self.protein = protein
-        self.carbs = carbs
+    def __init__(self, amount=0, unit=''):
+        self.amount = amount
+        self.unit = unit
 
     def __repr__(self):
-        return 'Nutrition: {}'.format(self.id)
-
-class Category(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    products = db.relationship('Product', backref='category', lazy='dynamic')
-    recipes = db.relationship('Recipe', backref='category', lazy='dynamic')
-
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return 'Category: {}'.format(self.name)
+        return 'Ingredientlist: {}'.format(self.id)
 
