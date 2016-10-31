5241a0e6d2d8a5504ecc8d09cdb9a665877f00d0
Ubuntu <ubuntu@ip-172-31-35-219.us-west-2.compute.internal>
2016-10-31 17:11:35 +0000

    updated app/config.py for postgresql

--- app/config.py
+++ app/config.py
@@ -1,6 +1,5 @@
 import os
 basedir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = ''
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
