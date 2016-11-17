ed5d21fb04612e1871f7fffadb16c06fba2475ca
Lei Lin <kris_lin@utexas.edu>
2016-11-16 18:19:48 -0600

    changing index to home fixed #60

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from flask import jsonify, request, render_template
 import subprocess
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def index():
     return render_template('index.html')
 
