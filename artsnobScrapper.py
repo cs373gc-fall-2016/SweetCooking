c5214d2ff1f31a12bc9a1e49cda5bb78b6cbd356
Ubuntu <ubuntu@ip-172-31-35-219.us-west-2.compute.internal>
2016-11-21 14:08:12 +0000

    art scraper cleanup

--- artsnobScrapper.py
+++ artsnobScrapper.py
@@ -8,46 +8,6 @@ URL = 'http://artsnob.me:5000/api/'
 request = Request(URL + 'artist')
 
 f = open('jsonstuff.json', 'w')
-'''
-try:
-    response = urlopen(request)
-    data = response.read()
-    f.write(data)
-    # print(data)
-except URLError, e:
-    print('#### Error in Artist: ', e)
-
-
-request = Request(URL + 'artwork')
-try:
-    response = urlopen(request)
-    data = response.read()
-    f.write(data)
-    # print(data)
-except URLError, e:
-    print('#### Error in Artwork: ', e)
-
-
-
-request = Request(URL + 'style')
-try:
-    response = urlopen(request)
-    data = response.read()
-    f.write(data)
-    # print(data)
-except URLError, e:
-    print('#### Error in Style: ', e)
-
-
-request = Request(URL + 'collection')
-try:
-    response = urlopen(request)
-    data = response.read()
-    f.write(data)
-    # print(data)
-except URLError, e:
-    print('#### Error in Collection: ', e)
-'''
 
 response = urlopen(request)
 data = json.loads(response.read())
